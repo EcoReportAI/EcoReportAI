5a56a7651e18f242c9d0c0cdf851734e1ddc98e0
Kalu-Emeka <brickkalu@gmail.com>
2024-12-12 14:03:22 +0100

    Edited the text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ function Footer () {
                 <div className="grid grid-cols-3 gap-[170px] mx-[30px] sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 sm:gap-[20px] md:gap-[20px] lg:gap-[170px]"> 
                     <div className="flex flex-col">
                         <h2 className="text-white text-[18px] font-bold mb-[40px]">Contact</h2>
-                        <a className="text-[15px] mb-[20px]" href="">support@green-badge.com</a>
+                        <a className="text-[15px] mb-[20px]" href="">contact@green-badge.com</a>
                         <a className="text-[15px] mb-[20px]" href="">+2348028157761</a>
                     </div>  
                     <div className="flex flex-col">
@@ -22,7 +22,7 @@ function Footer () {
                         </div>
                     </div>  
                 </div>
-                <h2 className="text-center mt-[30px] sm:text-[15px] md:text-[15px] lg:text-[16px]">@Copyright 2023 (JoeConcepts Technologies). All Rights Reserved.</h2>
+                <h2 className="text-center mt-[30px] sm:text-[15px] md:text-[15px] lg:text-[16px]">@Copyright 2025 (Adsoft Online Ltd)</h2>
             </div>
         </>
     )
--- src/landingPage.tsx
+++ src/landingPage.tsx
@@ -17,22 +17,22 @@ const Home = () => {
         {
             id: 1,
             question: 'What is Green-Badge?',
-            answer:'A technology platform for certifying green projects, buying carbon credits and tracking sustainability with transparency.'
+            answer:'Green-Badge is a municipal-led sustainability platform that certifies, tracks and verifies impactful sustainability projects. It connects municipalities, project developers and investors to drive measurable environmental, social and economic progress.'
         },
         {
             id: 2,
-            question: 'How do I get started as a developer?',
-            answer:'Sign up, submit your project and start the certification process.'
+            question: 'How does the certification process work?',
+            answer:'Projects are certified by municipalities based on sustainability standards, with options for Verra or Gold Standard certification for marketplace listings.'
         },
         {
             id: 3,
-            question: 'How can I buy carbon credits?',
-            answer:'Browse verified projects and purchase credits that align with your goals.'
+            question: 'Who is Green-Badge for?',
+            answer:'Green-Badge is designed for Municipalities: To certify and track local sustainability projects Developers: To certify projects, gain recognition and attract investments. Investors: To fund verified projects or purchase carbon credits'
         },
         {
             id: 4,
-            question: 'How can municipalities use Green-Badge?',
-            answer:'Track city-wide sustainability, create policies and engage developers to meet goals.'
+            question: 'How can I get started with Green-Badge?',
+            answer:'Getting started is simple: Municipalities: Select basic plan or contact us for custom option. Developers: Submit your project for certification. Investors: Browse verified projects or buy carbon credits.'
         }
 
     ]
@@ -44,8 +44,8 @@ const Home = () => {
                 <div className="header pt-[60px] pl-[80px] text-black bg-gradient-to-b from-[#03FF95] to-[#011F29] sm:pl-[30px] md:pl-[30px] lg:pl-[80px]" data-aos="fade-down">
                     <div className='flex justify-center items-center flex-row sm:items-start md:items-start lg:items-center sm:flex-col md:flex-col lg:flex-row'>
                         <div className="pt-[40px] w-[70%] sm:w-[90%] md:w-[70%] lg:w-[70%]">
-                            <h2 className="h mb-2 text-[50px] text-white leading-[60px] font-bold mb-[30px] w-[85%]  sm:w-full md:w-[98%] lg:w-[85%] sm:text-[25px] md:text-[32px] sm:leading-[30px] md:leading-[40px] lg:text-[50px] lg:leading-[60px]">Empower Your Green Projects with Verified Impact</h2>
-                            <p className='w-[85%] text-white font-bold text-bold text-[17px] mb-7 sm:text-[15px] md:text-[16px] lg:text-[16px] sm-full md:w-full lg:w-[85%] sm:text-[13px] md:text-[14px] lg:text-[17px]'>Green-Badge empowers municipaities, project owners and investors to collaborate on sustainable initiatives, offering project certification, real-time impact tracking and a trusted marketplace for green investments to drive measurable environmental, social and economic progress.</p>
+                            <h2 className="h mb-2 text-[50px] text-white leading-[60px] font-bold mb-[30px] w-[90%]  sm:w-full md:w-[98%] lg:w-[90%] sm:text-[25px] md:text-[32px] sm:leading-[30px] md:leading-[40px] lg:text-[50px] lg:leading-[60px]">Empowering Municipalities for Certified Sustainability and Verified Impact</h2>
+                            <p className='w-[85%] text-white font-bold text-bold text-[17px] mb-7 sm:text-[15px] md:text-[16px] lg:text-[16px] sm-full md:w-full lg:w-[85%] sm:text-[13px] md:text-[14px] lg:text-[17px]'>Green-Badge connects municipalities, project owners, and investors to accelerate sustainable initiatives. We offer project certification, real-time impact tracking and a trusted marketplace for green investments to drive measurable environmental, social and economic progress.</p>
                             <div className='text-[14px] sm:mb-[20px] md:mb-[20px] lg:mb-0'>
                                 <a href="/" rel="noopener noreferrer"><button  className="px-[20px] py-[5px] border-none mr-[20px] text-white bg-gradient-to-r from-[#03FF95] to-[#008890] rounded-[4px]" type="button">Get Started</button></a>
                                 <a className="text-[14px] mr-[20px]" href="#works" rel="noopener noreferrer"><button  className="px-[20px] py-[5px] border border-[#03FF95] text-[#008890] bg-[#FFFFFF] rounded-[4px] hover:bg-gray-300" type="button">Learn More...</button></a>
@@ -138,9 +138,9 @@ const Home = () => {
                         <h2 className="font-bold text-[25px] mb-[10px]">Get Started!</h2>
                         <p className="mb-[10px]">Let's accelerate the Net Zero Journey</p>
                         <div className='text-[12px] flex w-[120%] sm:flex-col md:flex-col lg:flex-row'>
-                            <a href="/" rel="noopener noreferrer"><button  className="px-[20px] py-[5px] border-none mr-[20px] text-white bg-gradient-to-r from-[#03FF95] to-[#008890] rounded-[4px] sm:mb-[10px] md:mb-[10px] lg:mb-0" type="button">For Municipal</button></a>
-                            <a href="/" rel="noopener noreferrer"><button  className="px-[20px] py-[5px] border-none mr-[20px] text-white bg-gradient-to-r from-[#03FF95] to-[#008890] rounded-[4px] sm:mb-[10px] md:mb-[10px] lg:mb-0" type="button">Project Developer</button></a>
-                            <a href="/" rel="noopener noreferrer"><button  className="px-[20px] py-[5px] border-none mr-[20px] text-white bg-gradient-to-r from-[#03FF95] to-[#008890] rounded-[4px] sm:mb-[10px] md:mb-[10px] lg:mb-0" type="button">Impact Investor</button></a>
+                            <a href="/register" rel="noopener noreferrer"><button  className="px-[20px] py-[5px] border-none mr-[20px] text-white bg-gradient-to-r from-[#03FF95] to-[#008890] rounded-[4px] sm:mb-[10px] md:mb-[10px] lg:mb-0" type="button">For Municipal</button></a>
+                            <a href="/register" rel="noopener noreferrer"><button  className="px-[20px] py-[5px] border-none mr-[20px] text-white bg-gradient-to-r from-[#03FF95] to-[#008890] rounded-[4px] sm:mb-[10px] md:mb-[10px] lg:mb-0" type="button">Project Developer</button></a>
+                            <a href="/register" rel="noopener noreferrer"><button  className="px-[20px] py-[5px] border-none mr-[20px] text-white bg-gradient-to-r from-[#03FF95] to-[#008890] rounded-[4px] sm:mb-[10px] md:mb-[10px] lg:mb-0" type="button">Impact Investor</button></a>
                         </div>
                         </div>
                         <img className="w-[30%] object-contain visible sm:invisible md:invisible lg:visible" src="/images/FB_IMG_16953304614928263 34 (1).webp" alt="" />
@@ -188,11 +188,12 @@ const Home = () => {
                             <div className="flex justify-center flex-col items-center">
                                 <h2 className="font-bold text-[#14192B] text-[20px]">For Developers</h2>
                                 <p className="text-[#4B5262] text-[15px] mt-[10px]">Customer Reach & Budget</p>
-                                <h2 className="font-bold text-[#14192B] text-[35px] my-[20px]">$49<span className="font-normal text-[18px]">/Month</span></h2>
+                                <h2 className="font-bold text-[#14192B] text-[35px] my-[20px]">$20<span className="font-normal text-[18px]">/Starter</span></h2>
                                 <hr className="w-full" />
                                 <ul className="text-black text-[14px] space-y-2 mb-[20px] ml-[20px] px-[30px] mt-[30px]">
-                                    <li className="list-disc"><strong>Free:</strong> Basic project submission and documentation.</li>
-                                    <li className="list-disc"><strong>Premium Plan:</strong> Includes advanced analytics and boosted visibility.</li>
+                                    <li className="list-disc">Municipal certification fee</li>
+                                    <li className="list-disc">Optional certifications (e.g., Verra, Gold Standard).</li>
+                                    <li className="list-disc">Access to advanced analytics and boosted project visibility.</li>
                                 </ul>
                                 <a href="/" rel="noopener noreferrer"><button  className="px-[45px] py-[13px] my-[30px] border-none text-white bg-gradient-to-r from-[#143E14] to-[#6FB432] rounded-[10px]" type="button">Get Started</button></a>
                             </div>
@@ -201,24 +202,25 @@ const Home = () => {
                             <div className="flex justify-center flex-col items-center text-white">
                                 <h2 className="font-bold text-[20px]">For Impact Investors</h2>
                                 <p className="text-[15px] mt-[10px]">Customer Reach & Budget</p>
-                                <h2 className="font-bold text-[35px] my-[20px]">$199<span className="font-normal text-[18px]">/Month</span></h2>
+                                <h2 className="font-bold text-[35px] my-[20px]">$0<span className="font-normal text-[18px]">/Starter</span></h2>
                                 <hr className="w-full" />
                                 <ul className="text-[14px] space-y-2 mb-[20px] ml-[20px] px-[30px] mt-[30px]">
                                     <li className="list-disc"><strong>Pay-As-You-Go:</strong> Purchase carbon credits on-demand.</li>
                                     <li className="list-disc"><strong>Corporate Sustainablitiy Plans:</strong> Custom packages for large-scale carbon footprint offsets.</li>
                                 </ul>
-                                <a href="/" rel="noopener noreferrer"><button  className="px-[45px] py-[13px] mb-[30px] border-none text-[#29591B] bg-white rounded-[10px] sm:mb-[20px] md:mb-[20px] lg:mb-[35px]" type="button">Get Started</button></a>
+                                <a href="/" rel="noopener noreferrer"><button  className="px-[45px] py-[13px] my-[30px] border-none text-[#29591B] bg-white rounded-[10px] sm:my-[20px] md:my-[20px] lg:my-[35px]" type="button">Get Started</button></a>
                             </div>
                         </div>
                         <div className="rounded-md pt-[20px] w-[25%] mr-[40px] shadow-md border-[#03FF95] border sm:mb-[20px] md:mb-[20px] lg:mb-0 sm:mr-0 md:mr-0 lg:mr-[40px] sm:w-[90%] md:w-[50%] lg:w-[25%]">
                             <div className="flex justify-center flex-col items-center">
                                 <h2 className="font-bold text-[#14192B] text-[20px]">For Municipalities</h2>
                                 <p className="text-[#4B5262] text-[15px] mt-[10px]">Customer Reach & Budget</p>
-                                <h2 className="font-bold text-[#14192B] text-[35px] my-[20px]">$250<span className="font-normal text-[18px]">/Month</span></h2>
+                                <h2 className="font-bold text-[#14192B] text-[35px] my-[20px]">$500<span className="font-normal text-[18px]">/Month</span></h2>
                                 <hr className="w-full" />
                                 <ul className="text-black text-[14px] mb-[10px] ml-[20px] px-[30px] mt-[30px]">
-                                    <li className="list-disc"><strong>Basic Plan:</strong> Certify a limited number of projects annually.</li>
-                                    <li className="list-disc"><strong>Enterprise Plan:</strong> Advanced tools for tracking, impact reporting and project management.</li>
+                                    <li className="list-disc"><strong>Basic:</strong> Project submission and certification.</li>
+                                    <li className="list-disc"><strong>Enterprise:</strong> Advanced tools for tracking, impact reporting and project management.</li>
+                                    <li className="list-disc">Access to Municipal Green investment and support packages.</li>
                                 </ul>
                                 <a href="/" rel="noopener noreferrer"><button  className="px-[45px] py-[13px] my-[20px] border-none text-white bg-gradient-to-r from-[#143E14] to-[#6FB432] rounded-[10px] sm:my-[20px] md:my-[20px] lg:my-[25px]" type="button">Get Started</button></a>
                             </div>
